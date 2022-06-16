d060a036fdcab89d292ba18f159c2ad4e88643ca
Spacekido <spacekido.kido@gmail.com>
2022-06-16 18:51:17 +0200

    Testo pagina principale

--- src/Main.svelte
+++ src/Main.svelte
@@ -6,25 +6,54 @@
 
 <TopAppBar />
 <main>
-  <div class="mdc-typography--headline2 uppercase">Introduzione all'amianto</div>
-  <LayoutGrid>
+  <div class="mdc-typography--headline2 uppercase">Sai davvero cos'é l'amianto?</div>
+  <div class="mdc-typography--headline5">Sai come ha rovinato le vite di innumerevoli persone?</div>
+  <div class="mdc-typography--headline5">Sai quanti innocenti sono morti inutilmente?</div>
+  <div class="section1">
+    <div class="mdc-typography--headline4 uppercase">COS'É L'AMIANTO E PERCHÉ VIENE SCAVATO?</div>
+    <div class="mdc-typography--body1">
+      Con il termine <span class="bold">AMIANTO</span> (o asbesto) viene identificato un minerale naturale a struttura fibrosa, conosciuto sin dall'antichitá
+      per le sue ottime proprietá termoisolanti e fonoassorbenti. Nel XX secolo, grazie alla sua economicitá di PRODUZIONE, é stato largamente adottato
+      dall'industria di tutto il mondo in innumerevoli applicazioni industriali ed edilizie. Con il tempo però tale materiale si è rivelato
+      <span class="bold">NOCIVO</span> per la salute dell'uomo, a causa del rilascio di fibre che, se inalate, possono provocare patologie gravi ed irreversibili
+      all'apparato respiratorio (asbestosi, carcinoma polmonare e mesoteliomi).
+    </div>
+
+    <div class="mdc-typography--headline4 uppercase primary">PERCHÉ É ANCORA IN USO SEBBENE SIA LETALE?</div>
+    <div class="mdc-typography--body1">
+      Negli anni '70 é stato raggiunto il <span class="bold">PICCO</span> di produzione di amianto, coinvolgendo 85 paesi diversi nella sua estrazione e
+      lavorazione. In Italia, dal 1992 ad oggi, sono entrate in vigore numerose leggi che <span class="bold">VIETANO</span> estrazione, commercializzazione e
+      produzione di amianto. Ma non tutte le nazioni hanno bandito questo materiale; Russia, Cina, Brasile e Canada sono gli attuali maggiori
+      <span class="bold">PRODUTTORI</span> di amianto.
+    </div>
+  </div>
+
+  <div class="section2" style="background-color: #202020;">
+    <div class="mdc-typography--headline5">
+      DOVE L'AMIANTO HA AVUTO LE <span class="primary">PEGGIORI CONSEGUENZE?</span>
+    </div>
+  </div>
+
+  <!-- <LayoutGrid>
     <Cell>
       <Card />
     </Cell>
     <Cell>
       <Card />
     </Cell>
-  </LayoutGrid>
+  </LayoutGrid> -->
 </main>
 
 <style>
   main {
-    padding: 1em;
+    padding: 0;
     text-align: center;
     max-width: 240px;
-    margin-top: 130px;
+    margin: 100px 0;
   }
-
+  .section1 {
+    margin: 600px 300px;
+  }
   /* TODO aggiungere altri media breakpoint */
   @media (min-width: 640px) {
     main {
