e56081f1db355a2904bf0d32caec1c969a4cd517
Spacekido <spacekido.kido@gmail.com>
2022-06-24 12:44:23 +0200

    Updates

--- src/Introduzione/Title.svelte
+++
@@ -1,5 +0,0 @@
-<div class="mdc-typography--headline2 uppercase">Sai davvero cos'é l'amianto?</div>
-<div class="mdc-typography--headline4">
-  Sai come ha rovinato le vite di innumerevoli persone?
-  <br />Sai quanti innocenti sono morti inutilmente?
-</div>
